9361d658fcb44737094dc1a5b15163c4c8122334
DarkblooM-SR <darkbloompro@gmail.com>
2022-12-10 23:54:26 +0100

    updated help

--- genpsswd.py
+++ genpsswd.py
@@ -16,12 +16,12 @@ def psswd(length: int, exclude: str | None) -> str:
     return word
 
 @click.command()
-@click.option("--length", "-l", help="Number of characters to be included in password (default: 16)", default=16, required=False)
-@click.option("--count", "-c", help="Number of passwords to generate (default: 1)", default=1, required=False)
+@click.option("--length", "-l", help="Number of characters to be included in password (default: 16).", default=16, required=False)
+@click.option("--count", "-c", help="Number of passwords to generate (default: 1).", default=1, required=False)
 @click.option("--exclude", "-e", help="String representing a list of characters not to include in the password.", required=False)
 def generate(length: int, count: int, exclude: str | None) -> None:
     '''
-    Generates one or multiple random password(s)
+    Generates one or multiple random password(s).
     '''
     for _ in range(count):
         print(psswd(length, exclude))
